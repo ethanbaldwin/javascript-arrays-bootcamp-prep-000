1d25589da417166ed683b367ca2b233f76658d12
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-08-27 21:08:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element);
   return array;
