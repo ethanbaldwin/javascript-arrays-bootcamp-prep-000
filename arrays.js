070bf7a2f1c45ea8fc94520fda66120cbdc982c5
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-08-27 20:52:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array,element) {
   
-}
+};
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  
+};
+
