10fa66d31255ee709864556757793326e86b52b9
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-08-27 21:06:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,10 @@
-function addElementToBeginningOfArray(array,element) {
-  
-};
-
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  
-};
+  array.unshift(element);
+  return array;
+}
+
+function addElementToBeginningOfArray(array,element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
 
